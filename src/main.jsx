4cd34d640dcde1885b89b7615a38e710de581db6
Sankirthan Sangarapu <ssangarapu@gmail.com>
2025-07-14 00:24:26 +0530

    File name change

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import UserContext from './context/UserContext.jsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/Store.js'
+import { store } from './redux/stores.js'
 
 
 createRoot(document.getElementById('root')).render(
